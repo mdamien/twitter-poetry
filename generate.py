3a64ee5a1c7a9124a8a94a11adf609b7e6671c28
Bensiin Borukhov <benicorp@Bensiins-MacBook-Pro.local>
2014-06-01 02:04:47 -0400

    added db, reverse n-grams

--- generate.py
+++ generate.py
@@ -1,7 +1,12 @@
 import sqlite3, random
 from operator import itemgetter
 from preprocess import end_sent
-DB_LOC = "/Users/benicorp/Dropbox/EmilyTweets/db/emily_ngrams.db"
+DB_LOC = "./emily_ngrams.db"
+
+# from nltk.corpus import names
+# name_list = set([name.lower() for name in names.words()])
+name_list = []
+i_words = set(['i', "i'm", "i'll", "i've", "i'd"])
 
 def choose_word_ahead(prev1, prev2, prev3=None):
     choose = random.random()
@@ -30,55 +35,55 @@ def choose_word_back(next1, next2, next3=None):
         res = ''
         if next3:
             try:
-                cur.execute("select word1 from quadgrams where word2=? and word3=? and word4=? and summed_prob>=? order by summed_prob asc limit 1;", (next1, next2, next3, choose))
+                cur.execute("select word4 from reverse_quadgrams where word1=? and word2=? and word3=? and summed_prob>=? order by summed_prob asc limit 1;", (next1, next2, next3, choose))
                 res = cur.fetchone()[0]
             except TypeError:
                pass
         if not res:
             try:
-                cur.execute("select word1 from trigrams where word2=? and word3=? and summed_prob>=? order by summed_prob asc limit 1;", (next1, next2, choose))
+                cur.execute("select word3 from reverse_trigrams where word1=? and word2=? and summed_prob>=? order by summed_prob asc limit 1;", (next1, next2, choose))
                 res = cur.fetchone()[0]
             except TypeError:
-                 cur.execute("select word1 from bigrams where word2=? and summed_prob>=? order by summed_prob asc limit 1;", (next1, choose))
+                 cur.execute("select word2 from reverse_bigrams where word1=? and summed_prob>=? order by summed_prob asc limit 1;", (next1, choose))
                  res = cur.fetchone()[0]
     return res
     
-def combined_forward(seed_word, randomize=True):
-    sent = []
-    word1 = ''
-    word2 = ''
-    word3 = ''
-    with sqlite3.connect(DB_LOC) as con:
-        cur = con.cursor()
-        if randomize:
-            cur.execute("select * from trigrams where word1=?", (seed_word,))
-            choose = random.random()
-            current = cur.next()
-            try:
-                while current[-1] <= choose:
-                    current = cur.next()
-            except StopIteration:
-                pass
-        else:
-            cur.execute("select * from trigrams where word1=? order by probability desc limit 1;", (seed_word,))
-            current = cur.next()
-        word1 = current[0]
-        word2 = current[1]
-        word3 = current[2]
-        sent.append(word1)
-        sent.append(word2)
-        sent.append(word3)
-        prev_word1 = word1
-        word1, word2 = word2, word3
-        while word3 not in end_sent:
-            if word.isalpha():
-                word3 = choose_word_ahead(word1, word2)
-                sent.append(word3)
-                word1, word2 = word2, word3
-            else:
-                word3 = choose_word_ahead(prev_word1, word1, word2)
-                word1, word2 = word2, word3
-    return sent
+# def combined_forward(seed_word, randomize=True):
+#     sent = []
+#     word1 = ''
+#     word2 = ''
+#     word3 = ''
+#     with sqlite3.connect(DB_LOC) as con:
+#         cur = con.cursor()
+#         if randomize:
+#             cur.execute("select * from trigrams where word1=?", (seed_word,))
+#             choose = random.random()
+#             current = cur.next()
+#             try:
+#                 while current[-1] <= choose:
+#                     current = cur.next()
+#             except StopIteration:
+#                 pass
+#         else:
+#             cur.execute("select * from trigrams where word1=? order by probability desc limit 1;", (seed_word,))
+#             current = cur.next()
+#         word1 = current[0]
+#         word2 = current[1]
+#         word3 = current[2]
+#         sent.append(word1)
+#         sent.append(word2)
+#         sent.append(word3)
+#         prev_word1 = word1
+#         word1, word2 = word2, word3
+#         while word3 not in end_sent:
+#             if word.isalpha():
+#                 word3 = choose_word_ahead(word1, word2)
+#                 sent.append(word3)
+#                 word1, word2 = word2, word3
+#             else:
+#                 word3 = choose_word_ahead(prev_word1, word1, word2)
+#                 word1, word2 = word2, word3
+#     return sent
     
 def trigrams_forward(seed_word, randomize=True):
     sent = []
@@ -120,7 +125,7 @@ def trigrams_back(seed_word, randomize=True):
     with sqlite3.connect(DB_LOC) as con:
         cur = con.cursor()
         if randomize:
-            cur.execute("select * from trigrams where word3=?", (seed_word,))
+            cur.execute("select * from reverse_trigrams where word1=?", (seed_word,))
             choose = random.random()
             current = cur.next()
             try:
@@ -129,96 +134,96 @@ def trigrams_back(seed_word, randomize=True):
             except StopIteration:
                 pass
         else:
-            cur.execute("select * from trigrams where word3=? order by probability desc limit 1;", (seed_word,))
-            current = cur.next()
-        word3 = current[2]
-        sent.insert(0, word3)
-        word2 = current[1]
-        sent.insert(0, word2)
-        word1 = current[0]
-        sent.insert(0, word1)
-        word2, word3 = word1, word2
-        done = False
-        while not done:
-            word1 = choose_word_back(word2, word3)
-            if not word1[-1].isdigit():
-                sent.insert(0, word1)
-                word2, word3 = word1, word2
-            else:
-                done = True
-    return sent
-    
-def quadgrams_forward(seed_word, randomize=True):
-    sent = []
-    word1 = ''
-    word2 = ''
-    word3 = ''
-    word4 = ''
-    with sqlite3.connect(DB_LOC) as con:
-        cur = con.cursor()
-        if randomize:
-            cur.execute("select * from quadgrams where word1=?", (seed_word,))
-            choose = random.random()
-            current = cur.next()
-            try:
-                while current[-1] <= choose:
-                    current = cur.next()
-            except StopIteration:
-                pass
-        else:
-            cur.execute("select * from quadgrams where word1=? order by probability desc limit 1;", (seed_word,))
+            cur.execute("select * from reverse_trigrams where word1=? order by probability desc limit 1;", (seed_word,))
             current = cur.next()
         word1 = current[0]
         word2 = current[1]
         word3 = current[2]
-        word4 = current[3]
         sent.append(word1)
         sent.append(word2)
         sent.append(word3)
-        sent.append(word4)
-        while word4 not in end_sent:
-            word4 = choose_word_ahead(word1, word2, word3)
-            sent.append(word4)
-            word1, word2, word3 = word2, word3, word4
-    return sent
-    
-def quadgrams_back(seed_word, randomize=True):
-    sent = []
-    word1 = ''
-    word2 = ''
-    word3 = ''
-    word4 = ''
-    with sqlite3.connect(DB_LOC) as con:
-        cur = con.cursor()
-        if randomize:
-            cur.execute("select * from quadgrams where word4=?", (seed_word,))
-            choose = random.random()
-            current = cur.next()
-            try:
-                while current[-1] <= choose:
-                    current = cur.next()
-            except StopIteration:
-                pass
-        else:
-            cur.execute("select * from quadgrams where word1=? order by probability desc limit 1;", (seed_word,))
-            current = cur.next()
-        word1 = current[0]
-        word2 = current[1]
-        word3 = current[2]
-        word4 = current[3]
-        sent.append(word1)
-        sent.append(word2)
-        sent.append(word3)
-        sent.append(word4)
         done = False
         while not done:
-            word1 = choose_word_back(word2, word3, word4)
-            if not word1[-1].isdigit():
-                sent.insert(0, word1)
-                word2, word3, word4 = word1, word2, word3
+            word3 = choose_word_back(word1, word2)
+            if not word3[-1].isdigit():
+                sent.append(word3)
+                word1, word2 = word2, word3
             else:
                 done = True
+        sent.reverse()
     return sent
+    
+# def quadgrams_forward(seed_word, randomize=True):
+#     sent = []
+#     word1 = ''
+#     word2 = ''
+#     word3 = ''
+#     word4 = ''
+#     with sqlite3.connect(DB_LOC) as con:
+#         cur = con.cursor()
+#         if randomize:
+#             cur.execute("select * from quadgrams where word1=?", (seed_word,))
+#             choose = random.random()
+#             current = cur.next()
+#             try:
+#                 while current[-1] <= choose:
+#                     current = cur.next()
+#             except StopIteration:
+#                 pass
+#         else:
+#             cur.execute("select * from quadgrams where word1=? order by probability desc limit 1;", (seed_word,))
+#             current = cur.next()
+#         word1 = current[0]
+#         word2 = current[1]
+#         word3 = current[2]
+#         word4 = current[3]
+#         sent.append(word1)
+#         sent.append(word2)
+#         sent.append(word3)
+#         sent.append(word4)
+#         while word4 not in end_sent:
+#             word4 = choose_word_ahead(word1, word2, word3)
+#             sent.append(word4)
+#             word1, word2, word3 = word2, word3, word4
+#     return sent
+#     
+# def quadgrams_back(seed_word, randomize=True):
+#     sent = []
+#     word1 = ''
+#     word2 = ''
+#     word3 = ''
+#     word4 = ''
+#     with sqlite3.connect(DB_LOC) as con:
+#         cur = con.cursor()
+#         if randomize:
+#             cur.execute("select * from quadgrams where word4=?", (seed_word,))
+#             choose = random.random()
+#             current = cur.next()
+#             try:
+#                 while current[-1] <= choose:
+#                     current = cur.next()
+#             except StopIteration:
+#                 pass
+#         else:
+#             cur.execute("select * from quadgrams where word1=? order by probability desc limit 1;", (seed_word,))
+#             current = cur.next()
+#         word1 = current[0]
+#         word2 = current[1]
+#         word3 = current[2]
+#         word4 = current[3]
+#         sent.append(word1)
+#         sent.append(word2)
+#         sent.append(word3)
+#         sent.append(word4)
+#         done = False
+#         while not done:
+#             word1 = choose_word_back(word2, word3, word4)
+#             if not word1[-1].isdigit():
+#                 sent.insert(0, word1)
+#                 word2, word3, word4 = word1, word2, word3
+#             else:
+#                 done = True
+#     return sent
     
 def eval_size_words(sent_list):
     scored = {}
@@ -282,13 +287,18 @@ def eval_lm(sent_list):
     
 def rejoin(word_list):
     padded = []
+    first = True
     for w in word_list:
-        
+        if first or w in name_list or w in i_words:
+            w=w.title()
+            first = False
         padded.append(w)
         if w[-1].isalpha():
             padded.insert(-1, " ")
     if padded[-1] == " ":
         padded = padded[:-1]
+    if padded[-1] == ";":
+        padded[-1] = "."
     return ''.join(padded)[1:]
     
 def generate_sentence(seed, tag, num_candidates=5, iters=20, randomize=True):
@@ -308,7 +318,9 @@ def generate_sentence(seed, tag, num_candidates=5, iters=20, randomize=True):
                 following.append(candidate_next)
         except TypeError:
             pass
+    print prev
     prev = eval_size_chars(prev, max_len/2)[:-1]
+    print prev
     prev_index = (len(prev)-num_candidates)/2
     if prev_index > 0:
         prev=prev[prev_index:-prev_index]
--- ngrams.py
+++ ngrams.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 import sqlite3
-DB_LOC = "/Users/benicorp/Dropbox/EmilyTweets/db/emily_ngrams.db"
+DB_LOC = "./emily_ngrams.db"
 
 def count_unigrams(sent_list):
     unigrams = defaultdict(int)
@@ -25,6 +25,20 @@ def count_bigrams(sent_list):
         cur.execute("create table bigram_counts(word1 text, word2 text, num int);")
         cur.executemany("insert into bigram_counts values(?,?,?)", [x+(bigrams[x],) for x in bigrams])
         
+def reverse_count_bigrams(sent_list):
+    bigrams = defaultdict(int)
+    for sent in sent_list:
+        sent = sent[:]
+        sent.reverse()
+        pairs = [tuple(sent[i:i+2]) for i in range(len(sent)-1)]
+        for pair in pairs:
+            bigrams[pair] += 1
+    with sqlite3.connect(DB_LOC) as con:
+        cur = con.cursor()
+        cur.execute("drop table if exists reverse_bigram_counts;")
+        cur.execute("create table reverse_bigram_counts(word1 text, word2 text, num int);")
+        cur.executemany("insert into reverse_bigram_counts values(?,?,?)", [x+(bigrams[x],) for x in bigrams])
+        
 def count_trigrams(sent_list):
     trigrams = defaultdict(int)
     for sent in sent_list:
@@ -37,6 +51,20 @@ def count_trigrams(sent_list):
         cur.execute("create table trigram_counts(word1 text, word2 text, word3 text, num int);")
         cur.executemany("insert into trigram_counts values(?,?,?, ?)", [x+(trigrams[x],) for x in trigrams])
         
+def reverse_count_trigrams(sent_list):
+    trigrams = defaultdict(int)
+    for sent in sent_list:
+        sent = sent[:]
+        sent.reverse()
+        triples = [tuple(sent[i:i+3]) for i in range(len(sent)-2)]
+        for triple in triples:
+            trigrams[triple] += 1
+    with sqlite3.connect(DB_LOC) as con:
+        cur = con.cursor()
+        cur.execute("drop table if exists reverse_trigram_counts;")
+        cur.execute("create table reverse_trigram_counts(word1 text, word2 text, word3 text, num int);")
+        cur.executemany("insert into reverse_trigram_counts values(?,?,?, ?)", [x+(trigrams[x],) for x in trigrams])
+        
 def count_quadgrams(sent_list):
     quadgrams = defaultdict(int)
     for sent in sent_list:
@@ -48,6 +76,20 @@ def count_quadgrams(sent_list):
         cur.execute("drop table if exists quadgram_counts;")
         cur.execute("create table quadgram_counts(word1 text, word2 text, word3 text, word4 text, num int);")
         cur.executemany("insert into quadgram_counts values(?,?,?,?,?)", [x+(quadgrams[x],) for x in quadgrams])
+        
+def reverse_count_quadgrams(sent_list):
+    quadgrams = defaultdict(int)
+    for sent in sent_list:
+        sent = sent[:]
+        sent.reverse()
+        fours = [tuple(sent[i:i+4]) for i in range(len(sent)-3)]
+        for four in fours:
+            quadgrams[four] += 1
+    with sqlite3.connect(DB_LOC) as con:
+        cur = con.cursor()
+        cur.execute("drop table if exists reverse_quadgram_counts;")
+        cur.execute("create table reverse_quadgram_counts(word1 text, word2 text, word3 text, word4 text, num int);")
+        cur.executemany("insert into reverse_quadgram_counts values(?,?,?,?,?)", [x+(quadgrams[x],) for x in quadgrams])
         
 def unigram_probs():
     with sqlite3.connect(DB_LOC) as con:
@@ -75,6 +117,21 @@ def bigram_probs():
         cur.execute("drop table if exists bigram_probs;")
         cur.execute("create table bigram_probs (word1 text, word2 text, probability float);")
         cur.executemany("insert into bigram_probs values(?,?,?)", [x+(bigrams[x],) for x in bigrams])
+
+def reverse_bigram_probs():
+    with sqlite3.connect(DB_LOC) as con:
+        cur = con.cursor()
+        bigrams = {}
+        cur.execute("select * from unigram_counts;")
+        unigrams = {}
+        for v in cur:
+            unigrams[v[0]] = float(v[1])
+        cur.execute("select * from reverse_bigram_counts;")
+        for v in cur:
+            bigrams[(v[0],v[1])] = v[2]/unigrams[v[0]]
+        cur.execute("drop table if exists reverse_bigram_probs;")
+        cur.execute("create table reverse_bigram_probs (word1 text, word2 text, probability float);")
+        cur.executemany("insert into reverse_bigram_probs values(?,?,?)", [x+(bigrams[x],) for x in bigrams])
         
 def trigram_probs():
     with sqlite3.connect(DB_LOC) as con:
@@ -91,6 +148,21 @@ def trigram_probs():
         cur.execute("create table trigram_probs (word1 text, word2 text, word3 text, probability float);")
         cur.executemany("insert into trigram_probs values(?,?,?,?)", [x+(trigrams[x],) for x in trigrams])
         
+def reverse_trigram_probs():
+    with sqlite3.connect(DB_LOC) as con:
+        cur = con.cursor()
+        trigrams = {}
+        cur.execute("select * from reverse_bigram_counts;")
+        bigrams = {}
+        for v in cur:
+            bigrams[(v[0],v[1])] = float(v[2])
+        cur.execute("select * from reverse_trigram_counts;")
+        for v in cur:
+            trigrams[(v[0],v[1],v[2])] = v[3]/bigrams[(v[0],v[1])]
+        cur.execute("drop table if exists reverse_trigram_probs;")
+        cur.execute("create table reverse_trigram_probs (word1 text, word2 text, word3 text, probability float);")
+        cur.executemany("insert into reverse_trigram_probs values(?,?,?,?)", [x+(trigrams[x],) for x in trigrams])
+        
 def quadgram_probs():
     with sqlite3.connect(DB_LOC) as con:
         cur = con.cursor()
@@ -105,6 +177,21 @@ def quadgram_probs():
         cur.execute("drop table if exists quadgram_probs;")
         cur.execute("create table quadgram_probs (word1 text, word2 text, word3 text, word4 text, probability float);")
         cur.executemany("insert into quadgram_probs values(?,?,?,?,?)", [x+(quadgrams[x],) for x in quadgrams]) 
+        
+def reverse_quadgram_probs():
+    with sqlite3.connect(DB_LOC) as con:
+        cur = con.cursor()
+        quadgrams = {}
+        cur.execute("select * from reverse_trigram_counts;")
+        trigrams = {}
+        for v in cur:
+            trigrams[(v[0],v[1],v[2])] = float(v[3])
+        cur.execute("select * from reverse_quadgram_counts;")
+        for v in cur:
+            quadgrams[(v[0],v[1],v[2],v[3])] = v[4]/trigrams[(v[0],v[1],v[2])]
+        cur.execute("drop table if exists reverse_quadgram_probs;")
+        cur.execute("create table reverse_quadgram_probs (word1 text, word2 text, word3 text, word4 text, probability float);")
+        cur.executemany("insert into reverse_quadgram_probs values(?,?,?,?,?)", [x+(quadgrams[x],) for x in quadgrams]) 
         
 
 def sum_unigrams():
@@ -140,6 +227,25 @@ def sum_bigrams():
                 current.append(tuple((word1, word2, prob, current_sum)))
             cur.executemany("insert into bigrams values(?,?,?,?)", current)
             
+def reverse_sum_bigrams():
+    with sqlite3.connect(DB_LOC) as con:
+        cur = con.cursor()
+        cur.execute("drop table if exists reverse_bigrams;")
+        cur.execute("create table reverse_bigrams (word1 text, word2 text, probability real, summed_prob real);")
+        cur.execute("select * from unigram_probs;")
+        unigrams = [v[0] for v in cur]
+        for unigram in unigrams:
+            current = []
+            cur.execute("select * from reverse_bigram_probs where word1=?;",(unigram,))
+            current_sum = 0
+            for bigram in cur:
+                prob = bigram[-1]
+                current_sum += prob
+                word1 = bigram[0]
+                word2 = bigram[1]
+                current.append(tuple((word1, word2, prob, current_sum)))
+            cur.executemany("insert into reverse_bigrams values(?,?,?,?)", current)
+            
 def sum_trigrams():
     with sqlite3.connect(DB_LOC) as con:
         cur = con.cursor()
@@ -161,6 +267,28 @@ def sum_trigrams():
                 current.append(tuple((word1, word2, word3, prob, current_sum)))
             inserts.extend(current)
         cur.executemany("insert into trigrams values(?,?,?,?, ?)", inserts)
+        
+def reverse_sum_trigrams():
+    with sqlite3.connect(DB_LOC) as con:
+        cur = con.cursor()
+        cur.execute("drop table if exists reverse_trigrams;")
+        cur.execute("create table reverse_trigrams (word1 text, word2 text, word3 text, probability real, summed_prob real);")
+        cur.execute("select * from reverse_bigram_probs;")
+        bigrams = [tuple(v[:-1]) for v in cur]
+        inserts = []
+        for bigram in bigrams:
+            current = []
+            cur.execute("select * from reverse_trigram_probs where word1=? and word2=?;",(bigram))
+            current_sum = 0
+            for trigram in cur:
+                prob = trigram[-1]
+                current_sum += prob
+                word1 = trigram[0]
+                word2 = trigram[1]
+                word3 = trigram[2]
+                current.append(tuple((word1, word2, word3, prob, current_sum)))
+            inserts.extend(current)
+        cur.executemany("insert into reverse_trigrams values(?,?,?,?, ?)", inserts)
         
 def sum_quadgrams():
     with sqlite3.connect(DB_LOC) as con:
@@ -184,4 +312,27 @@ def sum_quadgrams():
                 current.append(tuple((word1, word2, word3, word4, prob, current_sum)))
             inserts.extend(current)
         cur.executemany("insert into quadgrams values(?,?,?,?,?,?)", inserts)
+        
+def reverse_sum_quadgrams():
+    with sqlite3.connect(DB_LOC) as con:
+        cur = con.cursor()
+        cur.execute("drop table if exists reverse_quadgrams;")
+        cur.execute("create table reverse_quadgrams (word1 text, word2 text, word3 text, word4 text, probability real, summed_prob real);")
+        cur.execute("select * from reverse_trigram_probs;")
+        trigrams = [tuple(v[:-1]) for v in cur]
+        inserts = []
+        for trigram in trigrams:
+            current = []
+            cur.execute("select * from reverse_quadgram_probs where word1=? and word2=? and word3=?;",(trigram))
+            current_sum = 0
+            for quadgram in cur:
+                prob = quadgram[-1]
+                current_sum += prob
+                word1 = quadgram[0]
+                word2 = quadgram[1]
+                word3 = quadgram[2]
+                word4 = quadgram[3]
+                current.append(tuple((word1, word2, word3, word4, prob, current_sum)))
+            inserts.extend(current)
+        cur.executemany("insert into reverse_quadgrams values(?,?,?,?,?,?)", inserts)
         
